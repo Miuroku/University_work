63f3dc2c757a66e2ab9cc229f1b26bac0aa0eb0a
Miuroku <thebestguy1101001@gmail.com>
2021-02-19 23:23:05 +0300

    refactor: add new folder mcha and its 1'st project

---
+++ 5_Term/mcha/lab1.py
@@ -0,0 +1,179 @@
+import numpy as np
+import numpy.linalg as la
+
+def debugOutput(a1_array, b1_array):
+    a_array = np.copy(a1_array)
+    b_array = np.copy(b1_array)
+    print(" a_array : ")
+    print2DArray(a_array)
+    print("\n b_array : ")
+    printArray(b_array)
+    print("\n")
+
+def print2DArray(arr):
+    for i in range(len(arr)):
+        print("     ", end='')        
+        for j in range(len(arr[i])):
+            print(str(arr[i][j]) + " ",end='')
+        print('')
+
+def printArray(arr):
+    print("     ", end='')        
+    for i in range(len(arr)):
+        print(str(arr[i]) + ' ', end='')
+
+# Checks if there's a possible solve.
+def checkSolve(a1_array, b1_array):
+    
+    a_array = np.copy(a1_array)
+    b_array = np.copy(b1_array)
+
+    n = len(a_array)
+    # Now we're crate Расширенную матрицу.
+    extended_matrix = np.column_stack((a_array, b_array))     
+    
+    if la.matrix_rank(extended_matrix) != la.matrix_rank(a_array):
+        print("@@@ Attention : данное СЛАУ не имеет решений !")
+        return False
+    elif la.matrix_rank(a_array) != n:
+        print("@@@ Attention : данное СЛАУ имеет бесконечность решений !")
+        return False
+    else:
+        return True
+
+# returns x_array as answer of ALU.
+def gauss1(a1_array, b1_array):
+
+    a_array = np.copy(a1_array)
+    b_array = np.copy(b1_array) 
+
+    n = len(b_array)
+    x_array = np.arange(n)
+    x_array = np.zeros_like(x_array, dtype=np.float64)
+    
+    # Here we'll get rectangle matrix.
+    # Прямой ход. Состоит из n-1 шага.
+    for k in range(0, n-1):                                 # k - номер итерации.      
+        if a_array[k][k] == 0:
+            print(f'1-st method could not be used, cuz A[{k}][{k}] = 0 on step ')
+            return False
+        else:
+            # Считаем главные элементы k-го шага.
+            # q считается 
+
+            for i in range(k+1, n):                           # i - номера уравненений.
+                q = a_array[i][k] / a_array[k][k]     
+                # from i-equation subtract (k-equation * q).
+                for c in range(n): # run on string.           
+                    a_array[i][c] -= a_array[k][c] * q                             
+                b_array[i] -= b_array[k] * q
+            print(f"iteration no. {k}")     
+            debugOutput(a_array, b_array)           
+
+    # Обратный ход. <--------------------------------------Error here somwhere .
+    # Иду от последней строки треугольной матрицы к первой. [n-1,0]. 
+    for ik in range(n-1, -1, -1):        
+        # x_array[k] = (b_array[k] - rightpart)/a_array[k][k].        
+        if ik == n-1:
+            x_array[ik] = (b_array[n-1]) / a_array[ik][ik]
+        else:
+            rightpart = 0
+            for k in range(ik, -1, -1): # <---------------- Here the cycle sucks .
+                rightpart += a_array[ik][k+1] * x_array[k+1]               
+            x_array[ik] = (b_array[ik] - rightpart) / a_array[ik][ik]
+
+        print(f"x_array[{ik}] = {x_array[ik]}")
+
+    return x_array
+
+def gauss2(a1_array, b1_array):
+
+    a_array = np.copy(a1_array)
+    b_array = np.copy(b1_array)
+
+    n = len(b_array)
+    x_array = np.arange(n)
+    x_array = np.zeros_like(x_array, dtype=np.float64)
+
+    # Here's a main logic.
+
+    return x_array
+
+def gauss3(a1_array, b1_array):
+
+    a_array = np.copy(a1_array)
+    b_array = np.copy(b1_array)
+
+    n = len(b_array)
+    x_array = np.arange(n)
+    x_array = np.zeros_like(x_array, dtype=np.float64)
+
+    # Here's a main logic.
+
+    return x_array
+
+# Data input.
+""" aSize = int(input("Enter an A dimension : "))
+print("Enter A array:")
+a_array = np.arange(aSize * aSize)
+a_array = a_array.reshape(aSize, aSize)
+a_array = np.zeros_like(a_array,dtype=np.float64)
+for i in range(aSize):
+    for j in range(aSize):
+        a_array[i][j] = float(input(f"A[{i}][{j}] = "))
+
+print("Enter B array : ")
+b_array = np.arange(aSize)
+b_array = np.zeros_like(b_array, dtype=np.float64)
+for i in range(aSize):
+    b_array[i] = float(input(f"B[{i}] = ")) """
+
+# Our data here.
+k = 14
+a_array = np.array(
+    [[ 5.13,	0.81,	3.47,	0.92,	-0.53],
+    [  -0.53,	5.13,	0.81,	3.47,	0.92],
+    [  3.72,	-0.53,	5.13,	0.81,	3.47],
+    [  0.67,	3.72,	-0.53,	5.13,	0.81],
+    [  0.81,	0.67,   3.72,   -0.53,	5.13]]
+)
+
+b_array = np.array(
+    [  4.2, 4.2, 4.2, 4.2, 4.2]
+)
+
+a_test = np.array(
+    [
+        [3., 2, -5],
+        [2, -1, 3],
+        [1, 2, -1]
+    ]
+)
+
+b_test = np.array(
+    [-1, 13, 9]
+)
+
+if checkSolve(a_test, b_test) == False:
+    print("")
+else:
+    print2DArray(a_array)
+    # 1-st gauss.
+    gaussAnswer = gauss1(a_test, b_test)
+    print(f"Our Answer 1: -------------------\n{gaussAnswer}")
+
+    # 2-nd gauss.
+
+    # 3-d gauss
+
+    # My A array:
+    # 5 x 5
+    # 5.13	0.81	3.47	0.92	-0.53
+    # -0.53	5.13	0.81	3.47	0.92
+    # 3.72	-0.53	5.13	0.81	3.47
+    # 0.67	3.72	-0.53	5.13	0.81
+    # 0.81	0.67	3.72	-0.53	5.13
+
+    # My B array: 
+    # 5
+    # 4,2   4,2  4,2  4,2  4,2 
