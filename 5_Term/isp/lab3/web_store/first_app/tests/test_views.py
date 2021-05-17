e8883850b460720582eb53400c639cbab00242ec
Miuroku <thebestguy1101001@gmail.com>
2021-05-17 22:44:22 +0300

    feat: add more tests

--- 5_Term/isp/lab3/web_store/first_app/tests/test_views.py
+++ 5_Term/isp/lab3/web_store/first_app/tests/test_views.py
@@ -1,12 +1,27 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from django.urls import reverse # for accessin urls by their names.
 from first_app.models import Product, Category
 
 class GetAboutUsViewTest(TestCase):
 
+    # 1)
     def test_view_url_exists_atdesired_location(self):
         resp = self.client.get('/about_us/')
         self.assertEquals(resp.status_code, 200)
+
+    # 2)
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse('about_us_url'))
+        self.assertEqual(resp.status_code, 200)
+    
+    # 3)
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('about_us_url'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'first_app/about_us.html')
+
 
 class GetSpecificProductViewTest(TestCase):
 
@@ -20,3 +35,25 @@ class GetSpecificProductViewTest(TestCase):
     def test_view_url_exists_atdesired_location(self):
         resp = self.client.get(f'/products/{Product.objects.get(pk=1)}/')
         self.assertEquals(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse('specific_product_url_with_slug', args=[Product.objects.get(pk=1)]))
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('specific_product_url_with_slug', args=[Product.objects.get(pk=1)]))        
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'first_app/specific_product.html')
+
+class GetUserProfilePageViewTest(TestCase):
+
+    def setUp(self):
+        # Create 2 users
+        test_user1 = User.objects.create(username='testuser1', password='12345')        
+        test_user1.save()
+        test_user2 = User.objects.create(username='testuser2', password='123456')        
+        test_user2.save()
+
+    def test_redirect_if_not_logged_in(self):
+        resp = self.client.get(reverse('user_profile_url'))
+        self.assertRedirects(resp, '/')    
