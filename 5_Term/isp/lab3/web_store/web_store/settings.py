a7a5f2e03f45b71db3569f2c10c7d1ee41c2dc10
Miuroku <thebestguy1101001@gmail.com>
2021-05-17 12:47:43 +0300

    fix: hide more sensitive data from settings.py

--- 5_Term/isp/lab3/web_store/web_store/settings.py
+++ 5_Term/isp/lab3/web_store/web_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 import my_config
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wmh^bdah^cre9uqz68p_71m(q6mfd!87p+u1)jw87+ezxg(ty5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
