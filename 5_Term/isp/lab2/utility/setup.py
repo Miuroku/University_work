79d1a571fc6e0ded0b560d4a987cfc0448c268ee
Miuroku <thebestguy1101001@gmail.com>
2021-06-07 08:39:42 +0300

    fix: fix setup files

--- 5_Term/isp/lab2/utility/setup.py
+++ 5_Term/isp/lab2/utility/setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='make_convertion',
-    packages=[
-        'objects_packager', 
-        'factory',
-        'serializers',
-    ],
+    # packages=[
+    #     'serializer_lib.objects_packager', 
+    #     'serializer_lib.factory',
+    #     'serializer_lib.serializers',
+    # ],
+    packages=find_packages(exclude=['tests']),
     version='0.2.0',
     description='Console serializer',
     author='Miuroku Turoma',
     install_requires=[],
-    scripts=['bin/make_convertion']
+    scripts=['bin/make_conversion']
 )
